7fc4b6f363c825f709ef9c6b9f18d4933178674b
Revxrsal <aloosh.kasasbeh@gmail.com>
2024-02-26 21:52:32 +0300

    add another URL

--- src/routes/auth/v1/authorize.ts
+++ src/routes/auth/v1/authorize.ts
@@ -1,8 +1,6 @@
 import type {APIEvent} from "@solidjs/start/server";
 import {redirect} from "@solidjs/router";
 
-export const URL = "https://xjaqtpgdbcsyxptpcqhy.supabase.co"
-
 export async function GET(event: APIEvent) {
-  throw redirect(`${URL}/v1/callback`, event.request)
+  throw redirect(`${URL}/v1/authorize`, event.request)
 }

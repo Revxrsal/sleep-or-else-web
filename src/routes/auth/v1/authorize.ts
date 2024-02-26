39659bd89949bcc0aff2c2f4a478a9c44f552b88
Revxrsal <aloosh.kasasbeh@gmail.com>
2024-02-26 21:57:40 +0300

    woops.

--- src/routes/auth/v1/authorize.ts
+++ src/routes/auth/v1/authorize.ts
@@ -1,6 +1,7 @@
 import type {APIEvent} from "@solidjs/start/server";
 import {redirect} from "@solidjs/router";
+import {API_URL} from "~/routes/auth/v1/callback";
 
 export async function GET(event: APIEvent) {
-  throw redirect(`${URL}/v1/authorize`, event.request)
+  throw redirect(`${API_URL}/v1/authorize`, event.request)
 }
--- src/routes/auth/v1/callback.ts
+++ src/routes/auth/v1/callback.ts
@@ -1,8 +1,8 @@
 import type {APIEvent} from "@solidjs/start/server";
 import {redirect} from "@solidjs/router";
 
-export const URL = "https://xjaqtpgdbcsyxptpcqhy.supabase.co"
+export const API_URL = "https://xjaqtpgdbcsyxptpcqhy.supabase.co"
 
 export async function GET(event: APIEvent) {
-  throw redirect(`${URL}/v1/callback`, event.request)
+  throw redirect(`${API_URL}/v1/callback`, event.request)
 }

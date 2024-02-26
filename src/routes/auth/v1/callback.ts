20dada7d552d66d1c13fa5ec9c6914830ec39b5d
Revxrsal <aloosh.kasasbeh@gmail.com>
2024-02-26 21:45:47 +0300

    try that

--- src/routes/auth/v1/callback.ts
+++ src/routes/auth/v1/callback.ts
@@ -2,5 +2,5 @@ import type {APIEvent} from "@solidjs/start/server";
 import {redirect} from "@solidjs/router";
 
 export async function GET(event: APIEvent) {
-  throw redirect("https://xjaqtpgdbcsyxptpcqhy.supabase.co/auth/v1/callback", event.request)
+  throw redirect("https://xjaqtpgdbcsyxptpcqhy.supabase.co/auth/v1/callback/", event.request)
 }

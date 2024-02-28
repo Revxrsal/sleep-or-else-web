f3cb13b1e9dd89a5a692b558f0a97c1ace3fd2ab
Revxrsal <aloosh.kasasbeh@gmail.com>
2024-02-28 11:45:48 +0300

    add option to customize the client key

--- src/database/client.ts
+++ src/database/client.ts
@@ -4,6 +4,6 @@ import {Database} from "~/database/database-types";
 export const SUPABASE_ANON_KEY: string = import.meta.env.VITE_SUPABASE_ANON_KEY
 export const SUPABASE_PROJECT_URL: string = import.meta.env.VITE_SUPABASE_PROJECT_URL
 
-export function createSupabaseClient() {
-  return createClient<Database>(SUPABASE_PROJECT_URL, SUPABASE_ANON_KEY)
+export function createSupabaseClient(key = SUPABASE_ANON_KEY) {
+  return createClient<Database>(SUPABASE_PROJECT_URL, key)
 }

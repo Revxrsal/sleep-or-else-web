4f30add218226855da091cdb373d069331e4357e
Revxrsal <aloosh.kasasbeh@gmail.com>
2024-03-05 23:41:35 +0300

    center stuff

--- src/routes/purchase-success.tsx
+++ src/routes/purchase-success.tsx
@@ -1,11 +1,13 @@
 import Header from "~/components/typography/Header";
 import Pg from "~/components/typography/Pg";
 import Column from "~/components/layout/Column";
+import PageTitle from "~/components/meta/PageTitle";
 
 export default function PurchaseSuccess() {
   return (
-    <main class={"mt-0"}>
+    <main class={"mt-0 center"}>
       <title>Purchase successful!</title>
+      <PageTitle>Purchase successful!</PageTitle>
       <Column>
         <Header>Purchase successful!</Header>
         <Pg class={"mx-12 text-xl"}>

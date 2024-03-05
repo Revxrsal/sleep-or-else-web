2d6cba5a64f95b703fb9d924f5fee07dd4478bac
Revxrsal <aloosh.kasasbeh@gmail.com>
2024-03-05 23:22:09 +0300

    a bit formatting

--- src/routes/support.tsx
+++ src/routes/support.tsx
@@ -1,7 +1,7 @@
 import PageTitle from "~/components/meta/PageTitle"
 import Header from "~/components/typography/Header"
 import Row from "~/components/layout/Row";
-import { FaBrandsDiscord } from "solid-icons/fa";
+import {FaBrandsDiscord} from "solid-icons/fa";
 
 const DISCORD_URL = "https://discord.com/invite/uwf72ZN"
 
@@ -10,8 +10,16 @@ export default function Support() {
     <main class={"center"}>
       <PageTitle>Support</PageTitle>
       <Header>Support</Header>
-      <Row class={"bg-blue-600 dark:bg-blue-600 p-8 rounded-full h-12 center select-none font-semibold text-white dark:text-white justify-between min-w-60 hover:brightness-90 cursor-pointer"}
-      onClick={() => window.open(DISCORD_URL, "_blank")}>
+      <Row
+        class={`
+        bg-blue-600 dark:bg-blue-600 
+        p-8 rounded-full h-12 center
+        select-none font-semibold 
+        text-white dark:text-white
+         justify-between min-w-60 
+         hover:brightness-90 cursor-pointer`
+        }
+        onClick={() => window.open(DISCORD_URL, "_blank")}>
         <FaBrandsDiscord size={32}/>
         Join our Discord
       </Row>
